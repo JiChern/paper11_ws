4ab26f19a82d63dc4c6d570e47b74f9191c72ed3
JiChern <ji.chenuk@gmail.com>
2024-05-11 17:18:22 +0800

    data

--- src/hexapod_control/scripts/brain.py
+++ src/hexapod_control/scripts/brain.py
@@ -40,7 +40,7 @@ class HexBrain(object):
         self.theta_sub = rospy.Subscriber('/current_theta', Float32MultiArray, self.theta_cb, queue_size=10)
         self.theta = np.zeros(6)
         self.kai = 1
-        self.ts = 5
+        self.ts = 10
 
         self.bezier = Bezier()
         self.bezier.addPoint(0,0)
@@ -78,36 +78,24 @@ class HexBrain(object):
         return smooth_mu
 
     
-    def gait_transition(self, current_gait, target_gait):
+    def gait_transition(self, current_gait, target_gait, progress):
         
         self.trans_finish = False
         smooth_theta = np.zeros(6)
+       
+        current_theta = gait_dict[current_gait]['theta']
+        current_mu = gait_dict[current_gait]['mu']
+        target_theta = gait_dict[target_gait]['theta']
+        target_mu = gait_dict[target_gait]['mu']
 
-        if not self.in_trans:
-            # Execute this function, start the transition, 
-            self.t_start_time = time.time()
-            self.in_trans = True
-        else:                                            
-            progress = time.time()-self.t_start_time
-            current_theta = gait_dict[current_gait]['theta']
-            current_mu = gait_dict[current_gait]['mu']
-            target_theta = gait_dict[target_gait]['theta']
-            target_mu = gait_dict[target_gait]['mu']
+        smooth_theta = self.smooth_theta(current_theta, target_theta,progress)
+        smooth_mu = self.smooth_mu(current_mu, target_mu,progress)
 
-            smooth_theta = self.smooth_theta(current_theta, target_theta,progress)
-            smooth_mu = self.smooth_mu(current_mu, target_mu,progress)
+        msg = Float32MultiArray()
+        msg.data = smooth_theta
+        self.theta_pub.publish(msg)
+        self.walker.mu = smooth_mu
 
-            msg = Float32MultiArray()
-            msg.data = smooth_theta
-            self.theta_pub.publish(msg)
-            self.walker.mu = smooth_mu
-
-            if progress > self.ts:
-                msg = Float32MultiArray()
-                msg.data = target_theta
-                self.theta_pub.publish(msg)
-                self.walker.mu = target_mu
-                self.in_trans = False
 
         return smooth_theta
     
@@ -125,7 +113,7 @@ if __name__ == '__main__':
 
     brain = HexBrain()
     data_folder = '/home/jichen/paper11_ws/src/hexapod_control/scripts/motor_data'
-    ds = DataSaver(data_folder,1)
+    ds = DataSaver(data_folder,2)
 
     hz = 200
     
@@ -152,6 +140,7 @@ if __name__ == '__main__':
 
     theta_vec = []
     duration_vec = []
+    mu_vec = []
 
     brain.walker.mu = gait_dict['wave']['mu']
 
@@ -172,19 +161,26 @@ if __name__ == '__main__':
 
         ds.dump_data(data)
 
-        if 10<duration<15 :
-            smooth_theta = brain.gait_transition('wave', 'tetra')
+        if 10<duration<20.5:
+            trans_start_time = 10
+            progress = duration-trans_start_time
+            smooth_theta = brain.gait_transition('wave', 'tetra', progress)
 
-        if 25<duration<30:
-            smooth_theta = brain.gait_transition('tetra', 'tri')
+        if 30<duration<40.5:
+            trans_start_time = 30
+            progress = duration-trans_start_time
+            smooth_theta = brain.gait_transition('tetra', 'tri', progress)
 
         print('duration: ',duration)
+
+        duration_vec.append(duration)
+        mu_vec.append(brain.walker.mu)
 
         # r.sleep()
         dt = time.time()-loop_start_time
 
-    # plt.figure()
-    # plt.plot(duration_vec, theta_vec)
-    # plt.show()
+    plt.figure()
+    plt.plot(duration_vec, mu_vec)
+    plt.show()
 
 
