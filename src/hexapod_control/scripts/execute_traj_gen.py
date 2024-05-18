659ef536e42042f355bf8d2a6771a7a7903a372e
JiChern <ji.chenuk@gmail.com>
2024-05-18 15:26:01 +0800

    data

--- src/hexapod_control/scripts/execute_traj_gen.py
+++ src/hexapod_control/scripts/execute_traj_gen.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     duration_vec = []
     mu_vec = []
 
-    start_gait = 'wave'
+    start_gait = 'metach'
     target_gait = 'tri'
 
     brain.walker.mu = gait_dict[start_gait]['mu']
