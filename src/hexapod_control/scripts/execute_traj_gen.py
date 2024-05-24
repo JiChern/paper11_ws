4ab889bd08aa450651b167e32485d83839d89331
JiChern <ji.chenuk@gmail.com>
2024-05-24 18:20:31 +0800

    data

--- src/hexapod_control/scripts/execute_traj_gen.py
+++ src/hexapod_control/scripts/execute_traj_gen.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 
     hz = 100
 
-    traj_index = 4
+    traj_index = 5
 
     rospy.init_node('gtexp2')
     walker = TrajGenerator(omega_0=2*np.pi, Hz=hz)
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     duration_vec = []
     mu_vec = []
 
-    start_gait = 'cater'
+    start_gait = 'tetra'
     target_gait = 'tri'
 
     brain.walker.mu = gait_dict[start_gait]['mu']
